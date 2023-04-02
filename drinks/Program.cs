2abcc4407465fb6cfe471cc8a9e8d63c93ae56c3
lainasguvna <123167091+lainasguvna@users.noreply.github.com>
2023-04-03 02:26:27 +0300

    Add files via upload

---
+++ drinks/Program.cs
@@ -0,0 +1,34 @@
+﻿var product = Console.ReadLine().ToLower();
+var TOWN = Console.ReadLine().ToLower();
+var quantity = double.Parse(Console.ReadLine());
+
+if (TOWN == "sofia")
+{
+    if (product == "cafe") Console.WriteLine(quantity * 0.50);
+    else if (product == "voda") Console.WriteLine(quantity * 1);
+    else if (product == "bira") Console.WriteLine(quantity * 2);
+    else if (product == "vodka") Console.WriteLine(quantity * 10);
+    else if (product == "cigari") Console.WriteLine(quantity * 5);
+    else if (product == "narga") Console.WriteLine(quantity * 15);
+    else Console.WriteLine("Invalid product");
+}
+else if (TOWN == "varna")
+{
+    if (product == "cafe") Console.WriteLine(quantity * 0.40);
+    else if (product == "voda") Console.WriteLine(quantity * 0.80);
+    else if (product == "bira") Console.WriteLine(quantity * 1.50);
+    else if (product == "vodka") Console.WriteLine(quantity * 9);
+    else if (product == "cigari") Console.WriteLine(quantity * 5.50);
+    else if (product == "narga") Console.WriteLine(quantity * 20);
+    else Console.WriteLine("Invalid product");
+}
+else if (TOWN == "burgas")
+{
+    if (product == "cafe") Console.WriteLine(0.50 * quantity * 0.60);
+    else if (product == "voda") Console.WriteLine(quantity * 1);
+    else if (product == "bira") Console.WriteLine(quantity * 1.50);
+    else if (product == "vodka") Console.WriteLine(quantity * 12);
+    else if (product == "cigari") Console.WriteLine(quantity * 5.50);
+    else if (product == "narga") Console.WriteLine(quantity * 17);
+    else Console.WriteLine("Invalid product");
+}
